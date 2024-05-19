d8ca410d5f1d96516a987dd0b038ee10571146dd
Chetaung23 <chetaung23@gmail.com>
2024-05-19 15:41:30 +0800

    Create footer.php
    added

---
+++ footer.php
@@ -0,0 +1,55 @@
+<footer class="container-fluid footerbg">
+    <div>
+        <div class="row">
+            <div class="col-md-3 d-flex justify-content-center flex-column">
+                <div class="p-2">
+                    <a class="footerlogo" href="#">
+                        <img src="images/logo2.png" alt="HCWA Logo2">
+                    </a>
+                </div>
+                <div class="p-2">
+                    <p class="followus">Follow Us</p>
+                </div>
+                <div>
+                    <a class="socialmediaicons" href="#">
+                        <img src="images/facebookicon.png" alt="facebookicon">
+                    </a>
+                    <a class="socialmediaicons" href="#">
+                        <img src="images/xicon.png" alt="xicon">
+                    </a>
+                </div>
+            
+            </div>
+            <div class="col-md-9" style="margin:auto;"> 
+            <ul class="d-flex justify-content-around" style="margin:0;">
+                <a class="nav-linkfooter" href="#">About Us</a>
+                <a class="nav-linkfooter" href="#">Live Issues</a>
+                <a class="nav-linkfooter" href="#">Join Us</a>
+                <a class="nav-linkfooter" href="#">What's On</a>
+                <a class="nav-linkfooter" href="#">Contact Us</a>
+            </ul>
+            </div>
+        </div> <!-- row -->
+        <div style="padding:1%; opacity: 0.5;">
+            <hr color="white" size="1px"/>
+        </div>
+        <div class="row footerdetails">
+            <div class="col-md-3">
+                <a class="nav-linkfooter" href="#">
+                    <p>Privacy Policy</p>
+                </a>
+            </div>
+            <div class="col-md-6">
+                <p>© History Council of Western Australia. 2024</p>
+            </div>
+            <div class="col-md-3">
+                <a class="nav-linkfooter" href="#">
+                    <p >Terms and Conditions</p>
+                </a>
+            </div>
+        </div>
+    </div> <!-- container -->
+</footer> <!-- container-fluid -->
+<?php wp_footer(); ?>
+</body>
+</html>
