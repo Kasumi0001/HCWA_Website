a02c5ebb93011175d79bb58a875046629e68925a
Thisari134 <thisari.dharmaratne@gmail.com>
2024-06-03 12:32:56 +0800

    Editing Contact Us php
    Adding ACF

--- contact-us.php
+++ contact-us.php
@@ -8,22 +8,22 @@
 
 
     <div class="contactlanding"> 
-        <h1 class="contacttitle">CONTACT US</h1>
+        <h1 class="contacttitle"><?php the_field('titleforcontactus'); ?></h1>
         <div class="row contactsrow">
             <div class="col-md-12">
                 <div class="row">
                      <div class="col-4 contactemail">
-                        <p class="contactheading email">EMAIL ADDRESS</p>
-                        <a class="emaillink" href="#">office@historycouncilwa.org.au</a>
+                        <p class="contactheading email"><?php the_field('emailaddressheading'); ?></p>
+                        <a class="emaillink" href="mailto:office@historycouncilwa.org.au"><?php the_field('emailaddress'); ?></a>
                      </div>
                      <div class="col-4 contactsocials">
-                        <p class="contactheading socials">SOCIALS</p>
+                        <p class="contactheading socials"><?php the_field('socialsheading'); ?></p>
                         <a  href="https://www.facebook.com/Historycouncilwa/"><img class="contactfacebook" src="http://170.187.231.66/~meshatmurdoch3/wp-content/themes/HCWA_Website/images/contactfacebook.png"></a>
                         <a  href="https://x.com/wahistccl?s=21&t=hKX78WBRi0IVWx_gedaR3A"><img class="contacttwitter" src="http://170.187.231.66/~meshatmurdoch3/wp-content/themes/HCWA_Website/images/contacttwitter.png"></a>
                      </div>
                      <div class="col-4 contactaddress">
-                        <p class="contactheading address">ADDRESS PO BOX</p>
-                        <p class="addresstext">49 Broadway, Nedlands WA 6009</p>
+                        <p class="contactheading address"><?php the_field('addressheading'); ?></p>
+                        <p class="addresstext"><?php the_field('address'); ?></p>
                      </div>
                 </div>
             </div>
