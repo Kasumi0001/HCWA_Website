3335eff080871d1812039b5cb8c068547cd82493
Big-tismdotcom <zarahblight27@gmail.com>
2024-06-10 14:45:44 +0800

    Update whats-on.php

--- whats-on.php
+++ whats-on.php
@@ -22,7 +22,7 @@
 </header> <!-- header end -->
 
 <!-- ANNUAL LECTURE SECTION -->
-<section class="whiteborder">
+<section>
     <div class="row d-flex align-items-center">
         <div class="col-md-6">
             <h3 class="whatsontitledark"><?php the_field('titleforannuallecture'); ?></h3>
@@ -39,7 +39,7 @@
     <div class="custom-container">
         <div class="row">
             <div class="col-md-6">
-                <img src="<?php the_field('imageforawardsmain'); ?>" alt="Lecture">
+            <img class="img-fluid" src="<?php the_field('imageforawardsmain'); ?>" alt="Lecture">
                 </div>
             <div class="col-md-6">
                 <h3 class="awardstitle"><?php the_field('titleforawardsmain'); ?></h3>
@@ -50,27 +50,27 @@
 </section> <!-- container-fluid -->
 
 <!-- LIST OF AWARDS SECTION -->
-<section class="charcoalbg-whatson charcoalspacing">
+<section class="charcoalbg-whatson">
     <div class="row">
-        <div class="col-md-3">
+        <div class="col-md-6 col-lg-3 mb-4">
             <img class="listedawards" src="<?php the_field('imageforawards2024'); ?>" alt="awards 2024">
             <h4 class="awardsubtitle"><?php the_field('titleforawards2024'); ?></h4>
             <p class="awardslisttext"> <?php the_field('textforawards2024'); ?></p>
             <button type="button" class="btn btn-outline-secondary smallbutton awardbtn1" href="#">LEARN MORE</button> 
         </div>
-        <div class="col-md-3">
+        <div class="col-md-6 col-lg-3 mb-4">
             <img class="listedawards" src="<?php the_field('imageforawards2023'); ?>" alt="awards 2023">
             <h4 class="awardsubtitle"><?php the_field('titleforawards2023'); ?></h4>
             <p class="awardslisttext"> <?php the_field('textforawards2023'); ?></p>
             <button type="button" class="btn btn-outline-secondary smallbutton awardbtn2" href="#">LEARN MORE</button> 
         </div>
-        <div class="col-md-3">
+        <div class="col-md-6 col-lg-3 mb-4">
             <img class="listedawards" src="<?php the_field('imageforawards2022'); ?>" alt="awards 2022">
             <h4 class="awardsubtitle"><?php the_field('titleforawards2022'); ?></h4>
             <p class="awardslisttext"> <?php the_field('textforawards2022'); ?></p>
             <button type="button" class="btn btn-outline-secondary smallbutton awardbtn3" href="#">LEARN MORE</button> 
         </div>
-        <<div class="col-md-3">
+        <div class="col-md-6 col-lg-3 mb-4">
             <img class="listedawards" src="<?php the_field('imageforawards2021'); ?>" alt="awards 2021">
             <h4 class="awardsubtitle"><?php the_field('titleforawards2021'); ?></h4>
             <p class="awardslisttext"> <?php the_field('textforawards2021'); ?></p>
