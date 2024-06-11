c69c547e03e585489681aeb067d8a61e59f346cd
Big-tismdotcom <zarahblight27@gmail.com>
2024-06-11 21:19:08 +0800

    Update pastadvocacy-page-template.php

--- pastadvocacy-page-template.php
+++ pastadvocacy-page-template.php
@@ -7,44 +7,38 @@ Template Name: Past Advocacy
 <div class="container-fluid maincontainer">
     <div class="container">
         <div class="row">
-            <section class="maintext">              
-            <?php 
-$args = array(
-  'category_name' =>  'Past Advocacy',
- // 'posts_per_page'    =>   1
-);
-// the query
-$the_query = new WP_Query( $args ); ?>
- 
-<?php if ( $the_query->have_posts() ) : ?>
- 
-    <!-- pagination here -->
- 
-    <!-- the loop -->
-    <?php while ( $the_query->have_posts() ) : $the_query->the_post(); ?>
-<div class="thepost">
-<h2><?php the_title(); ?></h2> 
-    
-<?php
-                if ( has_post_thumbnail() ) { // check if the post has a Post Thumbnail assigned to it.
-                    the_post_thumbnail('thumbnail');
-                } ?>
-<p class="about-text"><?php the_excerpt(); ?></p>
-    <a class="readmore" href="<?php the_permalink(); ?>"> CONTINUE READING</a>
-</div>
-</div>    <?php endwhile; ?>
-    <!-- end of the loop -->
- 
-    <!-- pagination here -->
- 
-    <?php wp_reset_postdata(); ?>
- 
-<?php else : ?>
-    <p><?php _e( 'Sorry, no posts matched your criteria.' ); ?></p>
-<?php endif; ?>
-</section>
-           
-     </div>
+            <section class="maintext col-12">
+                <?php 
+                $args = array(
+                    'category_name' => 'Past Advocacy',
+                );
+                $the_query = new WP_Query( $args ); ?>
+                
+                <?php if ( $the_query->have_posts() ) : ?>
+                    <div class="row">
+                        <?php while ( $the_query->have_posts() ) : $the_query->the_post(); ?>
+                        <div class="col-md-4 mb-4">
+                            <div class="card h-100">
+                                <?php if ( has_post_thumbnail() ) : ?>
+                                <div class="card-img-top">
+                                    <?php the_post_thumbnail('medium', ['class' => 'img-fluid']); ?>
+                                </div>
+                                <?php endif; ?>
+                                <div class="card-body">
+                                    <h5 class="card-title"><?php the_title(); ?></h5>
+                                    <p class="card-text"><?php the_excerpt(); ?></p>
+                                    <a href="<?php the_permalink(); ?>" class="btn btn-primary">CONTINUE READING</a>
+                                </div>
+                            </div>
+                        </div>
+                        <?php endwhile; ?>
+                    </div>
+                    <?php wp_reset_postdata(); ?>
+                <?php else : ?>
+                    <p><?php _e( 'Sorry, no posts matched your criteria.' ); ?></p>
+                <?php endif; ?>
+            </section>
+        </div>
     </div>
-  </div>
+</div>
 <?php get_footer(); ?>
