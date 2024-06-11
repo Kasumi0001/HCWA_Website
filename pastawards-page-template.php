c79f9d12d1802a5aa8d75ddc72544092b0b2e2cb
Big-tismdotcom <zarahblight27@gmail.com>
2024-06-11 21:59:46 +0800

    Update pastawards-page-template.php

--- pastawards-page-template.php
+++ pastawards-page-template.php
@@ -7,44 +7,36 @@ Template Name: Past Awards
 <div class="container-fluid maincontainer">
     <div class="container">
         <div class="row">
-            <section class="maintext">              
+            <section class="maintext col-12">              
             <?php 
-$args = array(
-  'category_name' =>  'Past Awards',
- // 'posts_per_page'    =>   1
-);
-// the query
-$the_query = new WP_Query( $args ); ?>
+                $args = array(
+                    'category_name' => 'Past Awards',
+                    'posts_per_page' => -1 // Display all posts
+                );
+                $the_query = new WP_Query($args); 
+            ?>
  
-<?php if ( $the_query->have_posts() ) : ?>
- 
-    <!-- pagination here -->
- 
-    <!-- the loop -->
-    <?php while ( $the_query->have_posts() ) : $the_query->the_post(); ?>
-<div class="thepost">
-<h2><?php the_title(); ?></h2> 
-    
-<?php
-                if ( has_post_thumbnail() ) { // check if the post has a Post Thumbnail assigned to it.
-                    the_post_thumbnail('thumbnail');
-                } ?>
-<p class="about-text"><?php the_excerpt(); ?></p>
-    <a class="readmore" href="<?php the_permalink(); ?>"> CONTINUE READING</a>
-</div>
-</div>    <?php endwhile; ?>
-    <!-- end of the loop -->
- 
-    <!-- pagination here -->
- 
-    <?php wp_reset_postdata(); ?>
- 
-<?php else : ?>
-    <p><?php _e( 'Sorry, no posts matched your criteria.' ); ?></p>
-<?php endif; ?>
-</section>
-           
-     </div>
+            <?php if ($the_query->have_posts()) : ?>
+                <!-- the loop -->
+                <?php while ($the_query->have_posts()) : $the_query->the_post(); ?>
+                    <div class="thepost mb-4">
+                        <h2 class="post-title"><?php the_title(); ?></h2>
+                        <?php if (has_post_thumbnail()) : ?>
+                            <div class="post-thumbnail mb-3">
+                                <?php the_post_thumbnail('medium', ['class' => 'img-fluid']); ?>
+                            </div>
+                        <?php endif; ?>
+                        <p class="about-text"><?php the_excerpt(); ?></p>
+                        <a class="readmore btn btn-primary" href="<?php the_permalink(); ?>">CONTINUE READING</a>
+                    </div>
+                <?php endwhile; ?>
+                <!-- end of the loop -->
+                <?php wp_reset_postdata(); ?>
+            <?php else : ?>
+                <p><?php _e('Sorry, no posts matched your criteria.'); ?></p>
+            <?php endif; ?>
+            </section>
+        </div>
     </div>
-  </div>
+</div>
 <?php get_footer(); ?>
